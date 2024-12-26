31b89b4d3b29ee1f37a9805420ba8b8786cad310
HAK3R37 <kacperjahl37@gmail.com>
2024-12-26 21:19:04 +0100

    feat: API - Events management, Add missing AccessTokens in requests

--- src/clients/Api.client.ts
+++ src/clients/Api.client.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 import GetUsersRequestConfigBuilder from "../builders/api/GetUsersRequestConfig.builder";
 import DataStorage from "../storage/runtime/Data.storage";
+import { subscribe } from "diagnostics_channel";
+import SubscribeEventRequestConfigBuilder from "../builders/eventsub/SubscribeEventRequestConfig.builder";
+import TwitchEventId from "../enums/TwitchEventId.enum";
+import DeleteEventSubscriptionRequestConfigBuilder from "../builders/eventsub/DeleteEventSubscriptionRequestConfig.builder";
+import SubscribedEventListRequestConfigBuilder from "../builders/eventsub/SubscribedEventListRequestConfig.builder";
+import { CreateSubscriptionResponse, DeleteSubscriptionResponse, GetSubscriptionsResponse } from "../types/APIClient.types";
 
 export default class APIClient {
     private data: DataStorage;
@@ -10,11 +16,52 @@ export default class APIClient {
         this.data = DataStorage.getInstance();
     }
 
+    get events() {
+        return {
+            subscribe: async (type: TwitchEventId, version: 1 | 2, condition: any) : Promise<CreateSubscriptionResponse> => {
+                const requestConfig = new SubscribeEventRequestConfigBuilder()
+                    .setAccessToken(this.token)
+                    .setClientId(this.data.clientId.get() as string)
+                    .setSessionId(DataStorage.getInstance().websocketId.get() as string)
+                    .setType(type)
+                    .setVersion(version)
+                    .setCondition(condition)
+                    .build();
+                const response = await axios.request<CreateSubscriptionResponse>(requestConfig);
+                if(response.status !== 202) throw new Error(`Failed to subscribe to event type=${type}`);
+                return response.data;
+            },
+            unsubscribe: async (id: string) : Promise<DeleteSubscriptionResponse> => {
+                const requestConfig = new DeleteEventSubscriptionRequestConfigBuilder()
+                    .setAccessToken(this.token)
+                    .setClientId(this.data.clientId.get() as string)
+                    .setSubscriptionId(id)
+                    .build();
+                const response = await axios.request<DeleteSubscriptionResponse>(requestConfig);
+                if(response.status !== 204) throw new Error(`Failed to unsubscribe from event id=${id}`);
+                return response.data;
+            },
+            list: async (showUnactive: boolean = false) : Promise<GetSubscriptionsResponse> => {
+                const requestConfig = new SubscribedEventListRequestConfigBuilder()
+                    .setAccessToken(this.token)
+                    .setClientId(this.data.clientId.get() as string)
+                    .build();
+                const response = await axios.request<GetSubscriptionsResponse>(requestConfig);
+                if(response.status !== 200) throw new Error(`Failed to get event subscriptions`);
+                return {
+                    ...response.data,
+                    data: showUnactive ? response.data.data : response.data.data.filter(sub => sub.status === 'enabled')
+                };
+            }
+        }
+    }
+
     get user() {
         return {
             get: async (params: { ids?: string[], logins?: string[] }) : Promise<any> => {
                 const requestConfig = new GetUsersRequestConfigBuilder()
                     .setClientId(this.data.clientId.get() as string)
+                    .setAccessToken(this.token)
                     .addLogins(params.logins || [])
                     .addUserIds(params.ids || [])
                     .build();
@@ -26,6 +73,7 @@ export default class APIClient {
             getById: async (id: string) : Promise<any> => {
                 const requestConfig = new GetUsersRequestConfigBuilder()
                     .setClientId(this.data.clientId.get() as string)
+                    .setAccessToken(this.token)
                     .addUserId(id)
                     .build();
                 const response = await axios.request<ResponseBody<User>>(requestConfig);
