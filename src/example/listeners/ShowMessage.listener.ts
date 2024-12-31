43caed6aafddf87f56d668ec43916aa92d5fa1eb
HAK3R37 <kacperjahl37@gmail.com>
2024-12-31 17:58:26 +0100

    feat: ShowMessage example listener

---
+++ src/example/listeners/ShowMessage.listener.ts
@@ -0,0 +1,13 @@
+import { ChatListener } from "../../storage/decorators/ChatListener.decorator";
+import { ChatCommandExecutionData } from "../../types/ChatCommand.types";
+import { ChatListenerExecution } from "../../types/ChatListener.types";
+
+@ChatListener({
+    name: 'ShowMessage',
+    transient: true
+})
+export default class ShowMessageListener implements ChatListenerExecution {
+    async execution(data: ChatCommandExecutionData): Promise<void> {
+        console.log(`#${data.event.broadcaster_user_login} | ${data.event.chatter_user_login}: ${data.event.message.text}`);
+    }
+}
