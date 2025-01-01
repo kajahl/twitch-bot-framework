c539d98533b5b1e0b10655e54c7fb7fb10e5cded
HAK3R37 <kacperjahl37@gmail.com>
2025-01-01 06:11:13 +0100

    Import & Export for npm package

---
+++ index.ts
@@ -0,0 +1,96 @@
+// Main modules
+import TwitchBotFramework from './src/TwitchBotFramework';
+
+// Repository patterns
+import { ListenChannelsRepository } from './src/storage/repository/ListenChannels.repository';
+import { TokenRepository } from './src/storage/repository/Token.repository';
+
+// Predefined strategies
+import InMemoryTokenRepository from './src/storage/predefined/InMemoryToken.repository';
+
+// Twitch objects
+import Chat from './src/objects/Chat';
+import User from './src/objects/User';
+
+// Command interfaces
+import {  
+    ChatCommandExecutionGuard,
+    ChatCommandExecutionGuardAvaliableResults,
+    ChatCommandPreExecution,
+    ChatCommandExecution, 
+    ChatCommandPostExecution, 
+    ChatCommandExecutionData,
+} from './src/types/ChatCommand.types';
+
+// Listener interfaces
+import { 
+    ChatListenerExecution, 
+    ChatListenerExecutionData
+} from './src/types/ChatListener.types';
+
+// Example commands / listeners
+import PingCommand from './src/example/commands/Ping.command';
+import ExampleCommand from './src/example/commands/Example.command';
+import CounterListener from './src/example/listeners/Counter.listener';
+import ShowMessageListener from './src/example/listeners/ShowMessage.listener';
+
+// Decorator Types
+import { ChatCommandDecoratorOptions } from './src/types/ChatCommand.types';
+import { ChatListenerDecoratorOptions } from './src/types/ChatListener.types';
+
+// Other Types
+import ChannelChatMessageEventData, {
+    Message, MessageFragment, Badge, BadgeSetId
+} from './src/types/EventSub_Events/ChannelChatMessageEventData.types';
+
+// Export
+export default TwitchBotFramework;
+export {
+    ListenChannelsRepository, TokenRepository,
+    InMemoryTokenRepository,
+    Chat, User,
+    ChatCommandExecutionGuard, ChatCommandExecutionGuardAvaliableResults, ChatCommandPreExecution, ChatCommandExecution, ChatCommandPostExecution, ChatCommandExecutionData,
+    ChatListenerExecution, ChatListenerExecutionData,
+    ChatCommandDecoratorOptions, ChatListenerDecoratorOptions,
+    PingCommand, ExampleCommand,
+    CounterListener, ShowMessageListener,
+    ChannelChatMessageEventData, Message, MessageFragment, Badge, BadgeSetId
+}
+
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const clientId = process.env.CLIENT_ID as string;
+// const clientSecret = process.env.CLIENT_SECRET as string;
+// const userId = process.env.USER_ID as string;
+// const userRefreshToken = process.env.USER_REFRESH_TOKEN as string;
+
+// // ListenChannelsRepository
+
+// class ListenedChannels implements ListenChannelsRepository {
+//     private channels: string[] = ['87576158', '82197170'];
+//     async getChannelIds(): Promise<string[]> {
+//         return this.channels;
+//     }
+// }
+
+// // END ListenChannelsRepository
+
+// const app = new TwitchBotFramework({
+//     bot: {
+//         userId,
+//         clientId,
+//         clientSecret
+//     },
+//     channels: {
+//         listenChannelsClass: ListenedChannels,
+//         // listenChannels: ['87576158']
+//     },
+//     chat: {
+//         commands: [PingCommand, ExampleCommand],
+//         listeners: [CounterListener, ShowMessageListener]
+//     },
+//     repository: {
+//         tokenClass: InMemoryTokenRepository
+//     }
+// });
